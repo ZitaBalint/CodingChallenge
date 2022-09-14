5b1e77bb66d0c96e66634469163a3447429f7feb
ZitaBalint <zita.balint1072@gmail.com>
2022-09-14 09:26:11 +0000

    change the data in the const

---
+++ index.js
@@ -0,0 +1,22 @@
+const CARS = [ {
+    make : 'Aston Martin', 
+    price : '£50K', 
+    year :  '2012',
+},
+{
+    make : 'BMW',
+    price : '£30K',
+    year :  '2014',
+},
+{
+    make : 'Chevrolet',
+    price : '£20K',
+    year :  '2013',
+},
+{
+    make : 'Datsun',
+    price : '£2K',
+    year :  '2001',
+} ];
+
+function 
